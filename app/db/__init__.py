639b0f0cf6aacf9f538fd756a4152aee822f3903
Patrick M <patrickmacdonald07@gmail.com>
2023-02-09 17:37:03 -0500

    setting up views with db data

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -2,6 +2,7 @@ from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
+from flask import g
 
 load_dotenv()
 
@@ -9,3 +10,19 @@ load_dotenv()
 engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
+
+def init_db():
+    Base.metadata.create_all(engine)
+
+def get_db():
+    if 'db' not in g:
+        # store db connection in app context
+        g.db = Session()
+
+    return g.db
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
